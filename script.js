62f49d4966c95eefc6a0aa6b857618ab36752a04
Haoming Luo <hluo@mun.ca>
2024-01-21 14:40:07 -0330

    Draw the  game board

--- script.js
+++ script.js
@@ -1,18 +1,52 @@
 const board = document.getElementById('game-board');
-
+const gridSize = 20;
 let snake = [{ x: 10, y: 10 }];
+let food = generateFood();
+let direction = 'right';
 
 // Draw game map, snake, food
 function draw() {
     board.innerHTML = '';
     drawSnake();
+    drawFood();
 }
 
 // Draw snake
+// segment is position here 
 function drawSnake() {
     snake.forEach((segment) => {
         const snakeElement = createGameElement('div', 'snake');
-    }
-    )
+        setPositon(snakeElement, segment);
+        board.appendChild(snakeElement);
+    });
+}
+
+// Create a snake or food cube/div
+function createGameElement(tag, className) {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
+
+// Set the position of snake or food
+function setPositon(element, position) {
+    element.style.gridColumn = position.x;
+    element.style.gridRow = position.y;
+}
+
+function drawFood() {
+    const foodElement = createGameElement('div', 'food');
+    setPositon(foodElement, food)
+    board.appendChild(foodElement);
+}
+
+function generateFood() {
+    const x = Math.floor(Math.random() * gridSize) + 1;
+    const y = Math.floor(Math.random() * gridSize) + 1;
+    return { x, y };
+}
+
+function move() {
+
 
 }
